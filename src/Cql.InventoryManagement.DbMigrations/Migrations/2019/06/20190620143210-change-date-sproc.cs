5d9652f16ce4bd4ba0a2b68d04b62ac6e77177e7
Joseph Turnbull <joseph.turnbull@cqlcorp.net>
2019-06-20 14:37:35 -0400

    Sproc for changing renewal date is not integrated with Fluent Migrator

---
+++ src/Cql.InventoryManagement.DbMigrations/Migrations/2019/06/20190620143210-change-date-sproc.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cql.InventoryManagement.DbMigrations.Migrations._2019._06
+{
+   
+        [Migration(20190620141310)]
+        public class _20190620143210_change_date_sproc : Migration
+        {
+            public override void Down()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Up()
+            {
+            Execute.EmbeddedScript("Cql.InventoryManagement.DbMigrations.Resources.Sql.Stored-procedure.sql");
+        }
+        }
+    
+
+}
---
+++ src/Cql.InventoryManagement.DbMigrations/Resources/Sql/20190620143210-change-date-sproc.sql
@@ -0,0 +1,35 @@
+﻿
+Create PROCEDURE ChangeRenewal
+AS
+BEGIN
+	-- shows how many rows are affected
+	SET NOCOUNT OFF;
+
+	--rows start at 1 so the loop runs at least once
+	DECLARE @rows INT = 1;
+	--loop while rows is greater than 0 to update the program by adding the necessary number of months to the date
+	--if the renewal date is passed
+	WHILE (@rows > 0)
+	BEGIN
+		UPDATE Program
+			SET RenewalDate = DATEADD(MONTH, MonthsPerRenewal, RenewalDate)
+		WHERE RenewalDate < GETDATE()
+	
+		--setting rows to the number of rows changed so that the loop will keep going until there is no rows that were changed
+		SELECT @rows = @@ROWCOUNT
+	END
+
+	--same thing but this time with the plugin table
+	DECLARE @rows2 INT = 1;
+	
+	WHILE (@rows2 > 0)
+	BEGIN
+		UPDATE Plugins
+			SET RenewalDate = DATEADD(MONTH, MonthsPerRenewal, RenewalDate)
+		WHERE RenewalDate < GETDATE()
+	
+		SELECT @rows2 = @@ROWCOUNT
+	END
+
+	
+END
