fe798cebdbedfe0fd2364d6ea5e2b5336b6e76b3
Alex McDowell <alex.mcdowell@cqlcorp.com>
2019-06-24 15:54:36 -0400

    IIWA 102 - made table more generic

---
+++ src/frontend-admin/src/components/pages/HardwareListPage/MonitorsListPage.tsx
@@ -0,0 +1,204 @@
+import React, {useState, useEffect} from 'react'
+import {Route, Switch} from 'react-router-dom'
+import {sortTable} from '../../../utilities/quickSort'
+import {concatStyles as s} from '../../../utilities/mikesConcat'
+import {cloneDeep} from 'lodash'
+
+// Components
+import {FilteredSearch} from '../../reusables/FilteredSearch/FilteredSearch'
+import {Button} from '../../reusables/Button/Button'
+import {Group} from '../../reusables/Group/Group'
+import {Table} from '../../reusables/Table/Table'
+import icon from '../../../content/Images/CQL-favicon.png'
+
+// Styles
+import styles from './HardwareListPage.module.css'
+
+// Types
+interface IProgramsListPageProps {
+    history: any
+}
+
+//TODO: replace any w/ real type
+const initListData: any[] = []
+
+// Primary Component
+export const ProgramsListPage: React.SFC<IProgramsListPageProps> = props => {
+    const {history} = props
+    const [listData, setListData] = useState(initListData)
+    const [filtered, setFiltered] = useState(listData) //this is what is used in the list
+    const [search, setSearch] = useState('')
+    const [selected, setSelected] = useState({label: 'name', value: 'name'})
+
+    useEffect(() => {
+        //TODO: replace w/ real type
+        let data: any[] = []
+        //TODO: fetch data
+        setListData(data)
+    }, [setListData])
+
+    useEffect(() => {
+        // Search through listData based on current value
+        // of search bar and save results in filtered
+        let filteredTableInput = listData
+        filteredTableInput = listData.filter((row: any) => {
+            return (
+                row[selected.value]
+                    .toString()
+                    .toLowerCase()
+                    .search(search.toLowerCase()) !== -1
+            )
+        })
+        setFiltered(filteredTableInput)
+    }, [search, selected, listData])
+
+    const handleClick = () => {
+        history.push('/programs/new')
+    }
+
+    const handleRowClick = (name: string) => {
+        history.push(`/programs/${name}`)
+    }
+
+    const [rows, setRows] = useState([
+        ['Jira', '2020/08/24', 'Joe'],
+        ['Atlassian', '2020/08/24', 'Bill'],
+        ['Minecraft', '2020/08/24', 'Bob'],
+        ['WoW', '2020/08/24', 'Su z'],
+        ['League', '2020/08/24', 'Joseph'],
+        ['Office 365', '2020/08/24', 'Anne'],
+        ['Jira', '2020/08/24', 'Bob e'],
+        ['Atlassian', '2020/08/24', 'Janet'],
+        ['Minecraft', '2020/08/24', 'Maggie'],
+        ['WoW', '2020/08/24', 'Zion'],
+        ['League', '2020/08/24', 'Link'],
+        ['Office 365', '2020/08/24', 'Zelda'],
+    ])
+
+    const headerList = ['Peripherals', 'Purchase Date', 'Assigned to']
+    const headerStates = []
+    const headerStateCounts = []
+
+    //initialize all the header states and styling to be not sorted
+    for (let i = 0; i < headerList.length; i++) {
+        headerStates.push(styles.notSorted)
+        headerStateCounts.push(0)
+    }
+    var initHeaderStates = cloneDeep(headerStates)
+    var initHeaderStateCounts = cloneDeep(headerStateCounts)
+    var tempHeaderStates = cloneDeep(headerStates)
+    var tempHeaderStateCounts = cloneDeep(headerStateCounts)
+
+    var initState = {headerStates, headerStateCounts}
+    const [sortState, setSortState] = useState(initState)
+
+    function sortStates(index: number) {
+        if (sortState.headerStateCounts[index] == 0) {
+            tempHeaderStates[index] = styles.descending
+            tempHeaderStateCounts[index] = 1
+            setSortState({headerStates: tempHeaderStates, headerStateCounts: tempHeaderStateCounts})
+            tempHeaderStateCounts = [...initHeaderStateCounts]
+        } else if (sortState.headerStateCounts[index] == 1) {
+            tempHeaderStates[index] = styles.ascending
+            tempHeaderStateCounts[index] = 0
+            setSortState({headerStates: tempHeaderStates, headerStateCounts: tempHeaderStateCounts})
+            tempHeaderStateCounts = [...initHeaderStateCounts]
+        }
+    }
+
+    const renderHeaders = () => {
+        var headers = []
+
+        var firstHeader = (
+            <td
+                onClick={e => {
+                    setRows(sortTable(rows, 0, sortState.headerStateCounts[0]))
+                    sortStates(0)
+                }}
+            >
+                <div className={s(styles.header, styles.nameHeader)}>
+                    {headerList[0]}
+                    <div className={sortState.headerStates[0]} />
+                </div>
+            </td>
+        )
+        headers.push(firstHeader)
+
+        for (let i = 1; i < headerList.length; i++) {
+            let header = (
+                <td
+                    onClick={e => {
+                        setRows(sortTable(rows, 0, sortState.headerStateCounts[i]))
+                        sortStates(i)
+                    }}
+                >
+                    <div className={styles.header}>
+                        {headerList[i]}
+                        <div className={sortState.headerStates[i]} />
+                    </div>
+                </td>
+            )
+            headers.push(header)
+        }
+
+        return headers
+    }
+
+    function concatenatedDept(row: any[]) {
+        return (
+            <td className={styles.peripherals}>
+                <img className={styles.icon} src={icon} />
+                <div className={styles.alignLeft}>
+                    <text className={styles.peripheralName}>{row[0]}</text>
+                </div>
+            </td>
+        )
+    }
+
+    var renderedRows: any[] = []
+
+    rows.forEach(row => {
+        const transformedRow: any[] = []
+        for (let i = 0; i < row.length; i++) {
+            switch (i) {
+                case 0:
+                    transformedRow[0] = concatenatedDept(row)
+                case 1:
+                    transformedRow[1] = <td className={styles.alignLeft}>{row[1]}</td>
+                case 2:
+                    transformedRow[1] = <td className={styles.alignLeft}>{row[2]}</td>
+            }
+        }
+
+        renderedRows.push(transformedRow)
+    })
+
+    return (
+        <div className={styles.programsListMain}>
+            <Switch>
+                <Route path='/programs/:name' render={props => <div>{props.match.params.name} Detail Page</div>} />
+            </Switch>
+            <Group direction='row' justify='between'>
+                <Button text='Add' icon='add' onClick={handleClick} />
+
+                <FilteredSearch
+                    search={search}
+                    setSearch={setSearch}
+                    options={[
+                        //TODO: replace w/ real options
+                        {label: 'name', value: 'name'},
+                        {label: 'cost', value: 'cost'},
+                    ]}
+                    selected={selected}
+                    setSelected={setSelected}
+                />
+            </Group>
+
+            {/*<List />*/}
+
+            <div className={styles.page}>
+                <Table headers={renderHeaders()} rows={renderedRows} />
+            </div>
+        </div>
+    )
+}
