3c39dec4b8765087470068d3274b36c78970ce88
Charles Kornoelje <charles.kornoelje@cqlcorp.com>
2019-06-25 14:05:30 -0400

    IIWA-127: Add helper method to find is the user is an admin. Update some test data. Employee detail get is still WIP.

--- src/backend-api/Controllers/DetailController.cs
+++ src/backend-api/Controllers/DetailController.cs
@@ -1,8 +1,9 @@
 ﻿using backend_api.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,6 +19,28 @@ namespace backend_api.Controllers
         public DetailController(ITInventoryDBContext context)
         {
             _context = context;
+        }
+
+        private bool isAdmin()
+        {
+            //Take the token from the bearer header and split it from the bearer title
+            var TokenList = Request.Headers["Authorization"].ToString().Split(" ");
+            //turn stringifyed token into a JWT token
+            var JwtToken = new JwtSecurityTokenHandler().ReadJwtToken(TokenList[1]);
+            var username = JwtToken.Claims.First().Value;
+
+            bool isAdmin = false;
+            using (var adContext = new PrincipalContext(ContextType.Domain, "CQLCORP"))
+            {
+                var user = UserPrincipal.FindByIdentity(adContext, username);
+                if (user != null)
+                {
+                    string adGUID = user.Guid.ToString();
+                    isAdmin = _context.AuthIdserver.Where(x => x.ActiveDirectoryId == adGUID).First().IsAdmin;
+                }
+            }
+
+            return isAdmin;
         }
 
         // TODO: Abstract this reused code from this and the image controller.
@@ -61,82 +84,203 @@ namespace backend_api.Controllers
 
         }
 
+        /*
+         * GET: api/detail/employee/{id}
+         * Function returns the employee detail information.
+         * Returns: {
+         *      picture: partial URL (as string),
+         *      totalProgramCostPerMonth: decimal,
+         *      totalHardwareCost: decimal,
+         *      firstName: string,
+         *      lastName: string,
+         *      department: string,
+         *      role: string,
+         *      hireDate: date (as string),
+         *      hardware: [ {
+         *          id: int,
+         *          type: string,
+         *          make: string,
+         *          model: string,
+         *          serialNumber: string,
+         *          mfg: string,
+         *          purchaseDate: date (as string),
+         *          tooltip: {
+         *              ...
+         *              (assortedValues as strings or ints)
+         *          },
+         *      } ,.. ],
+         *      software: [ {
+         *          id: int,
+         *          name: string,
+         *          licenseKey: string, //TODO
+         *          costPerMonth: decimal,
+         *          flatCost: decimal,
+         *      } ,.. ],
+         *      licenses: [ {
+         *          id: int,
+         *          name: string,
+         *          licenseKey: string, //TODO
+         *          costPerMonth: decimal,
+         *          flatCost: decimal,
+         *      } ,.. ],
+         *  }            
+         */
         private IActionResult GetEmployeeDetail(int id)
         {
-            var emp = _context.Employee.Find(id);
+            bool isAdmin = this.isAdmin();
 
+            var emp = _context.Employee.Find(id);
             if (emp == null)
             {
                 return NotFound();
             }
-
-            // Make sure the thing is not deleted.
-            decimal totalHardwareCost = 0.0m;
-            List<object> hardware = new List<object>();
-            foreach (Server sv in _context.Server.Where(server => server.EmployeeId == id && server.IsDeleted == false))
+            else
             {
-                // TODO: add serial number? 
-                // TODO: add tool tip data.
-                object server = new { id = sv.ServerId, type = nameof(Server), sv.Make, sv.Model, sv.Mfg, sv.PurchaseDate };
-                hardware.Add(server);
-                totalHardwareCost += sv.FlatCost ?? 0.0m;
-            }
-            foreach (Computer cp in _context.Computer.Where(computer => computer.EmployeeId == id && computer.IsDeleted == false))
-            {
-                object computer = new { id = cp.ComputerId, type = nameof(Computer), cp.Make, cp.Model, cp.Mfg, cp.PurchaseDate };
-                hardware.Add(computer);
-                totalHardwareCost += cp.FlatCost ?? 0.0m;
-            }
-            foreach (Monitor mn in _context.Monitor.Where(monitor => monitor.EmployeeId == id && monitor.IsDeleted == false))
-            {
-                object monitor = new { id = mn.MonitorId, type = nameof(Monitor), mn.Make, mn.Model, mn.Mfg, mn.PurchaseDate };
-                hardware.Add(monitor);
-                totalHardwareCost += mn.FlatCost ?? 0.0m;
-            }
-            foreach (Peripheral pr in _context.Peripheral.Where(peripheral => peripheral.EmployeeId == id && peripheral.IsDeleted == false))
-            {
-                // TODO: Peripheral does not have make and model, but instead has name and type.
-                object peripheral = new { id = pr.PeripheralId, type = nameof(Peripheral), make = pr.PeripheralName, model = pr.PeripheralType, pr.Mfg, pr.PurchaseDate };
-                hardware.Add(peripheral);
-                totalHardwareCost += pr.FlatCost ?? 0.0m;
-            }
-
-            // Lists to return for programs.
-            List<object> software = new List<object>();
-            List<object> licenses = new List<object>();
-            decimal totalProgramCostPerMonth = 0.0m;
-
-            // For each program that is not deleted and is assigned to the employee
-            foreach (Models.Program prog in _context.Program.Where(prog => !prog.IsDeleted && prog.EmployeeId == id))
-            {
-                decimal costPerMonth = prog.ProgramCostPerYear / 12 ?? 0.0m;
-                totalProgramCostPerMonth += costPerMonth;
-                if (prog.IsLicense)
+                // Make sure the thing is not deleted.
+                decimal totalHardwareCost = 0.0m;
+                List<object> hardware = new List<object>();
+                foreach (Server sv in _context.Server.Where(server => server.EmployeeId == id && !server.IsDeleted))
                 {
-                    object license = new { prog.ProgramId, prog.ProgramName, prog.ProgramLicenseKey };
-                    licenses.Add(license);
+                    object tooltip = new
+                    {
+                        sv.NumberOfCores,
+                        sv.Ram,
+                        sv.LocalHHD,
+                    };
+                    object server = new
+                    {
+                        id = sv.ServerId,
+                        type = nameof(Server),
+                        sv.Make,
+                        sv.Model,
+                        sv.SerialNumber,
+                        sv.Mfg,
+                        sv.PurchaseDate,
+                        tooltip,
+                    };
+                    hardware.Add(server);
+                    totalHardwareCost += sv.FlatCost ?? 0.0m;
                 }
-                else
+                foreach (Computer cp in _context.Computer.Where(computer => computer.EmployeeId == id && !computer.IsDeleted))
                 {
-                    // TODO: How to check if a person is an admin? If so, then display the key.
-                    object sw = new { prog.ProgramId, prog.ProgramName, prog.ProgramLicenseKey, costPerMonth };
-                    software.Add(sw);
+                    object tooltip = new
+                    {
+                        cp.Cpu,
+                        cp.Ramgb,
+                        cp.Ssdgb,
+                    };
+                    object computer = new
+                    {
+                        id = cp.ComputerId,
+                        type = nameof(Computer),
+                        cp.Make,
+                        cp.Model,
+                        cp.SerialNumber,
+                        cp.Mfg,
+                        cp.PurchaseDate,
+                        tooltip,
+                    };
+                    hardware.Add(computer);
+                    totalHardwareCost += cp.FlatCost ?? 0.0m;
                 }
+                foreach (Monitor mn in _context.Monitor.Where(monitor => monitor.EmployeeId == id && !monitor.IsDeleted))
+                {
+                    object tooltip = new
+                    {
+                    };
+                    object monitor = new
+                    {
+                        id = mn.MonitorId,
+                        type = nameof(Monitor),
+                        mn.Make,
+                        mn.Model,
+                        mn.SerialNumber,
+                        mn.Mfg,
+                        mn.PurchaseDate,
+                        tooltip,
+                    };
+                    hardware.Add(monitor);
+                    totalHardwareCost += mn.FlatCost ?? 0.0m;
+                }
+                foreach (Peripheral pr in _context.Peripheral.Where(peripheral => peripheral.EmployeeId == id && !peripheral.IsDeleted))
+                {
+                    // NOTE: Peripheral does not have make and model, but instead has name and type.
+                    object tooltip = new
+                    {
+                    };
+                    object peripheral = new
+                    {
+                        id = pr.PeripheralId,
+                        type = nameof(Peripheral),
+                        make = pr.PeripheralName,
+                        model = pr.PeripheralType,
+                        pr.SerialNumber,
+                        pr.Mfg,
+                        pr.PurchaseDate,
+                        tooltip,
+                    };
+                    hardware.Add(peripheral);
+                    totalHardwareCost += pr.FlatCost ?? 0.0m;
+                }
+
+                // Lists to return for programs.
+                List<object> software = new List<object>();
+                List<object> licenses = new List<object>();
+                decimal totalProgramCostPerMonth = 0.0m;
+
+                // For each program that is not deleted and is assigned to the employee
+                foreach (Models.Program prog in _context.Program.Where(prog => !prog.IsDeleted && prog.EmployeeId == id))
+                {
+                    decimal costPerMonth = prog.ProgramCostPerYear / 12 ?? 0.0m;
+                    totalProgramCostPerMonth += costPerMonth;
+                    if (prog.IsLicense)
+                    {
+                        object license = new
+                        {
+                            id = prog.ProgramId,
+                            name = prog.ProgramName,
+                            licensesKey = isAdmin ? prog.ProgramLicenseKey : null,
+                            costPerMonth,
+                            flatCost = prog.ProgramFlatCost
+                        };
+                        licenses.Add(license);
+                    }
+                    else
+                    {
+                        object sw = new
+                        {
+                            id = prog.ProgramId,
+                            name = prog.ProgramName,
+                            licenseKey = isAdmin ? prog.ProgramLicenseKey : null,
+                            costPerMonth,
+                            flatCost = prog.ProgramFlatCost
+                        };
+                        software.Add(sw);
+                    }
+                }
+
+                // Partial path for picture
+                string picture = $"/images/employee/{id}";
+
+                // Get the department name
+                var department = _context.Department.Where(dep => dep.DepartmentId == emp.DepartmentId && !dep.IsDeleted).FirstOrDefault().DepartmentName;
+
+                object employeeDetail = new
+                {
+                    picture,
+                    totalProgramCostPerMonth,
+                    totalHardwareCost,
+                    emp.FirstName,
+                    emp.LastName,
+                    department,
+                    emp.Role,
+                    emp.HireDate,
+                    hardware,
+                    software,
+                    licenses,
+                };
+                return Ok(employeeDetail);
             }
-
-            string picture = $"/images/employee/{id}";
-
-            //// Combine the hardware into one list for the single table.
-            //List<object> hardware = (from x in servers select (Object)x).ToList();
-            //hardware.AddRange((from x in laptops select (Object)x).ToList());
-            //hardware.AddRange((from x in monitors select (Object)x).ToList());
-            //hardware.AddRange((from x in monitors select (Object)x).ToList());
-
-            // Get the department name
-            var department = _context.Department.Where(dep => dep.DepartmentId == emp.DepartmentId && !dep.IsDeleted).FirstOrDefault().DepartmentName;
-
-            object employeeDetail = new { picture, totalProgramCostPerMonth, totalHardwareCost, emp.FirstName, emp.LastName, emp.Role, department, hardware, licenses, software, };
-            return Ok(employeeDetail);
         }
 
     }
