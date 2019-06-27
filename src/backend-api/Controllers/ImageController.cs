4a46a3f0e1ee91d6117c8df31f2e885c8dcd3055
Charles Kornoelje <charles.kornoelje@cqlcorp.com>
2019-06-27 10:02:26 -0400

    IIWA-165: Chad's fix for getting the Uploaded Image variable and adding it as a DI. 🧙

--- src/backend-api/Controllers/ImageController.cs
+++ src/backend-api/Controllers/ImageController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Http;
 using backend_api.Helpers;
+using Microsoft.Extensions.Options;
 
 namespace backend_api.Controllers
 {
@@ -17,13 +18,12 @@ namespace backend_api.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
-        public IConfiguration Configuration { get; }
-        public string UploadedFileRootPath { get; set; }
-        public UploadController(IConfiguration configuration)
+        public UploadController(IOptions<UploadOptions> uploadOptions)
         {
-            Configuration = configuration;
-            UploadedFileRootPath = Configuration.GetSection("EnvironmentVariables").Get<EnvironmentVariables>().UploadedFileRootPath;
+            UploadOptions = uploadOptions;
         }
+
+        public IOptions<UploadOptions> UploadOptions { get; }
 
         private string[] models = new string[] { "employee", "department", "program", "server", "computer", "server", "monitor", "peripheral" };
 
@@ -61,7 +61,7 @@ namespace backend_api.Controllers
         public IActionResult GetPicture([FromRoute] string model, int id)
         {
             model = VerbatimMatch(model);
-            string imagePath = Path.Combine(UploadedFileRootPath, "images", model, $"{id}");
+            string imagePath = Path.Combine(UploadOptions.Value.UploadedFileRootPath, "images", model, $"{id}");
 
             // Check that the model name is valid.
             if (ValidModel(model))
@@ -103,7 +103,7 @@ namespace backend_api.Controllers
             if (ValidModel(model) && file != null && file.Length > 0)
             {
                 // Create the images folder if not already there.
-                string imagesPath = Path.Combine(UploadedFileRootPath, "images");
+                string imagesPath = Path.Combine(UploadOptions.Value.UploadedFileRootPath, "images");
                 Directory.CreateDirectory(imagesPath);
 
                 // Create model folder if not already there
