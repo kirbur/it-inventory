5e86c9079ccbac82f0c9d389867a1c34d7a2448d
Alex McDowell <alex.mcdowell@cqlcorp.com>
2019-06-28 14:59:22 -0400

    IIWA 154 - worked on getting data

--- src/frontend-admin/src/components/pages/DepartmentDetailPage/DepartmentDetailPage.tsx
+++ src/frontend-admin/src/components/pages/DepartmentDetailPage/DepartmentDetailPage.tsx
@@ -1,4 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import {AxiosService, URL} from '../../../services/AxiosService/AxiosService'
+
+//components
 import {Button} from '../../reusables/Button/Button'
 import {Group} from '../../reusables/Group/Group'
 import {DetailPageTable} from '../../reusables/DetailPageTable/DetailPageTable'
@@ -6,7 +9,14 @@ import {DropdownList} from '../../reusables/Dropdown/DropdownList'
 
 // Styles
 import styles from './DepartmentDetailPage.module.css'
-//import styles from './DepartmentDetailPage.module.css';
+
+// Context
+import {LoginContext} from '../../App/App'
+
+// Utils
+import {formatDate, getDays, calculateDaysEmployed} from '../../../utilities/FormatDate'
+import {format} from '../../../utilities/formatEmptyStrings'
+import {concatStyles as s} from '../../../utilities/mikesConcat'
 
 // Types
 interface IDepartmentDetailPageProps {
@@ -24,9 +34,73 @@ export const DepartmentDetailPage: React.SFC<IDepartmentDetailPageProps> = props
     const softwareHeaders = ['Software', '#', 'Cost']
     const licenseHeaders = ['License', 'CALs']
 
-    const [employeeRows, setEmployeeRows] = useState([])
-    const [softwareRows, setSoftwareRows] = useState([])
-    const [licenseRows, setLicenseRows] = useState([])
+    const [employeeRows, setEmployeeRows] = useState<any[]>([])
+    const [softwareRows, setSoftwareRows] = useState<any[]>([])
+    const [licenseRows, setLicenseRows] = useState<any[]>([])
+
+    const {
+        loginContextVariables: {accessToken, refreshToken /*, isAdmin*/},
+    } = useContext(LoginContext)
+    const axios = new AxiosService(accessToken, refreshToken)
+
+    useEffect(() => {
+        axios
+            .get(`/detail/department/${match.params.id}`)
+            .then((data: any) => {
+                console.log(data)
+                // let user: any = {
+                //     photo: data[0].picture,
+                //     name: data[0].firstName + ' ' + data[0].lastName,
+                //     department: data[0].department,
+                //     role: data[0].role,
+                //     hireDate: formatDate(data[0].hireDate),
+                //     hwCost: Math.round(data[0].totalHardwareCost * 100) / 100,
+                //     swCost: Math.round(data[0].totalProgramCostPerMonth * 100) / 100,
+                // }
+                // setUserData(user)
+
+                let e: any[] = []
+                // data[0].employees.map((i: any) =>
+                //     e.push([
+                //         format(i.name),
+                //         format(i.hireDate),
+
+                //     ])
+                // )
+                setEmployeeRows(e)
+
+                // var toolTipArray = []
+                // data[0].hardware.map((i: any) => toolTipArray.push(i.tooltip.cpu ? formatToolTip(i.tooltip) : ''))
+
+                let sw: any[] = []
+                // data[0].software.map((i: any) =>
+                // sw.push([
+                //     format(i.id),
+                //     format(i.name),
+                //     format(i.licenseKey),
+                //     format(Math.round(i.costPerMonth * 100) / 100),
+                //     format(i.flatCost),
+                // ])
+                // )
+                setSoftwareRows(sw)
+
+                let l: any[] = []
+                // data[0].licenses.map((i: any) =>
+                // l.push([
+                //     format(i.id),
+                //     format(i.name),
+                //     format(i.cals),
+                //     format(i.licenseKey),
+                //     format(Math.round(i.costPerMonth * 100) / 100),
+                //     format(i.flatCost),
+                // ])
+                // )
+                setLicenseRows(l)
+            })
+            .catch((err: any) => console.error(err))
+
+        //TODO: get dropdown content for all 3 dropdowns
+    }, [])
 
     return (
         <div className={styles.detailMain}>
