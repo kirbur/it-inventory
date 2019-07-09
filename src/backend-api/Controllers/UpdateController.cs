7aa15a912371dbfe0146609c51cd3ad0811fc606
Joseph Turnbull <joseph.turnbull@cqlcorp.com>
2019-07-09 14:41:51 -0400

    Program edit endpoints.

--- src/backend-api/Controllers/UpdateController.cs
+++ src/backend-api/Controllers/UpdateController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using backend_api.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -65,6 +66,107 @@ namespace backend_api.Controllers
             {
                 return BadRequest("Department does not exist or failed to supply ID");
             }
+        }
+        /* PUT: api/udpate/programall
+        * Will update all the rows of programs that have the given name
+        * Param input format:
+        * {     "Program" : {
+        *           "OldProgramName" : String,
+        *           "NewProgramName" : String,
+        *           "ProgramCostPerYear" : Decimal,
+        *           "ProgramFlatCost" : Decimal,
+        *           "ProgramLicenseKey" : String,
+        *           "IsLicense" : bool,
+        *           "ProgramDescription" : String,
+        *           "ProgramPurchaseLink" : String,
+        *           "DateBought" : DateTime,
+        *           "RenewalDate" : DateTime,
+        *           "MonthsPerRenewal" : int
+        *       }
+        * }
+        */
+
+        [HttpPut]
+        [Route("ProgramAll")]
+        public IActionResult EditPrograms([FromBody] UpdateProgramInputModel input)
+        {
+            try
+            {
+                foreach (var program in _context.Program.Where(x => x.ProgramName == input.Program.OldProgramName))
+                {
+                    program.ProgramName = input.Program.NewProgramName;
+                    program.ProgramCostPerYear = input.Program.ProgramCostPerYear;
+                    program.ProgramFlatCost = input.Program.ProgramFlatCost;
+                    program.ProgramLicenseKey = input.Program.ProgramLicenseKey;
+                    program.IsLicense = input.Program.IsLicense;
+                    program.Description = input.Program.ProgramDescription;
+                    program.ProgramPurchaseLink = input.Program.ProgramPurchaseLink;
+                    program.IsCostPerYear = input.Program.MonthsPerRenewal != null && input.Program.MonthsPerRenewal - 12 >= 0 ? true : false;
+                    program.DateBought = input.Program.DateBought;
+                    program.RenewalDate = input.Program.RenewalDate;
+                    program.MonthsPerRenewal = input.Program.MonthsPerRenewal;
+                }
+                _context.SaveChanges();
+                return StatusCode(202);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(error: e.Message);
+            }
+
+
+        }
+        /* PUT: api/udpate/program/{id}
+        * Will update the program identified by the id from the route
+        * Param input format:
+        * {     "Program" : {
+    	*            "ProgramName" : String,
+    	*            "ProgramCostPerYear" : Decimal,
+    	*            "ProgramFlatCost" : Decimal,
+    	*            "ProgramLicenseKey" : String,
+    	*            "ProgramDescription" : String,
+    	*            "ProgramPurchaseLink" : String,
+    	*            "DateBought" : DateTime,
+		*            "RenewalDate" : DateTime,
+		*            "MonthsPerRenewal" : int,
+		*            "EmployeeId" : int
+        *      }
+        * }
+        */
+
+        [HttpPut]
+        [Route("Program/{id}")]
+        public IActionResult EditProgram([FromBody] PostProgramInputModel input, [FromRoute] int id)
+        {
+            // Get program by ID.
+            var prog = _context.Program.Find(id);
+
+            if (prog != null)
+                try
+                {
+                    prog.ProgramCostPerYear = input.Program.ProgramCostPerYear;
+                    prog.ProgramFlatCost = input.Program.ProgramFlatCost;
+                    prog.ProgramLicenseKey = input.Program.ProgramLicenseKey;
+                    prog.Description = input.Program.ProgramDescription;
+                    prog.ProgramPurchaseLink = input.Program.ProgramPurchaseLink;
+                    prog.IsCostPerYear = input.Program.MonthsPerRenewal != null && input.Program.MonthsPerRenewal - 12 >= 0 ? true : false;
+                    prog.DateBought = input.Program.DateBought;
+                    prog.RenewalDate = input.Program.RenewalDate;
+                    prog.MonthsPerRenewal = input.Program.MonthsPerRenewal;
+                    prog.EmployeeId = input.Program.EmployeeId;
+
+                    _context.SaveChanges();
+                    return StatusCode(202);
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(error: e.Message);
+                }
+            else
+            {
+                return BadRequest("Program does not exist or failed to supply ID");
+            }
+
         }
     }
 }
