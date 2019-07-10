50185ce3999c46b0372340caf6d9b2ed43d5b433
Alex McDowell <alex.mcdowell@cqlcorp.com>
2019-07-10 10:46:26 -0400

    -

--- src/frontend-admin/src/components/pages/DepartmentsListPage/DepartmentsListPage.tsx
+++ src/frontend-admin/src/components/pages/DepartmentsListPage/DepartmentsListPage.tsx
@@ -45,6 +45,7 @@ export const DepartmentsListPage: React.SFC<IDepartmentsListPageProps> = props =
         axios
             .get('/list/departments')
             .then((data: any) => {
+                console.log(data)
                 var depts: any[] = []
                 data.map((i: any) =>
                     depts.push({
