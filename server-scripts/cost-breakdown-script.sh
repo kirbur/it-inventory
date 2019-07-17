ed0023c8e43644b477026216e5c4a89ab4db18c5
Charles Kornoelje <charles.kornoelje@cqlcorp.com>
2019-07-17 13:45:54 -0400

    IIWA-204: Add PowerShell scripts and readme.

--- server-scripts/cost-breakdown-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script to hit the costBreakdown endpoint.
-
-curl -k --request PATCH --header "token: $1" https://itinventory.cqlcorp.net/api/job/costbreakdown
--- server-scripts/low-resource-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script to hit the lowResource endpoint.
-
-curl -k --request PATCH --header "token: $1" https://itinventory.cqlcorp.net/api/job/lowresource
