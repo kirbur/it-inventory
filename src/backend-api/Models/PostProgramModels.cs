f8ab80d919fda459caf010af5525e84aa43600ec
Charles Kornoelje <charles.kornoelje@cqlcorp.com>
2019-07-19 10:31:59 -0400

    IIWA-211: Rename input classes.

---
+++ src/backend-api/Models/PostProgramModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace backend_api.Models
+{
+    // Object to get input from the request body.
+    public class PostProgramInputModel
+    {
+        public ProgramObjectModel Program { get; set; }
+    }
+
+    // Program object from the input
+    public class ProgramObjectModel
+    {
+        public ProgramObjectModel() { }
+        public int NumberOfPrograms { get; set; }
+        public string ProgramName { get; set; }
+        public decimal ProgramCostPerYear { get; set; }
+        public decimal ProgramFlatCost { get; set; }
+        public string ProgramLicenseKey { get; set; }
+        public bool IsLicense { get; set; }
+        public string Description { get; set; }
+        public string ProgramPurchaseLink { get; set; }
+        public DateTime DateBought { get; set; }
+        public DateTime? RenewalDate { get; set; }
+        public int? MonthsPerRenewal { get; set; }
+        public int? EmployeeId { get; set; }
+    }
+}
---
+++ src/backend-api/Models/PostProgramOverviewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace backend_api.Models
+{
+    // Class to get input from a request body.
+    public class UpdateProgramInputModel
+    {
+        public ProgramUpdateObjectModel Program { get; set; }
+    }
+
+    // Program object in the input from the request.
+    public class ProgramUpdateObjectModel
+    {
+        public string OldProgramName { get; set; }
+        public string ProgramName { get; set; }
+        public decimal? ProgramCostPerYear { get; set; }
+        public decimal? ProgramFlatCost { get; set; }
+        public string ProgramLicenseKey { get; set; }
+        public bool? IsLicense { get; set; }
+        public string Description { get; set; }
+        public string ProgramPurchaseLink { get; set; }
+        public DateTime? DateBought { get; set; }
+        public DateTime? RenewalDate { get; set; }
+        public int? MonthsPerRenewal { get; set; }
+    }
+}
