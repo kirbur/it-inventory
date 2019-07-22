9da78eb86dde629bd6af11855446b0a2c0ca1592
Alex McDowell <alex.mcdowell@cqlcorp.com>
2019-07-22 08:45:17 -0400

    IIWA 190 - fixed routing

--- src/frontend-admin/src/components/pages/DashboardPage/DashboardPage.tsx
+++ src/frontend-admin/src/components/pages/DashboardPage/DashboardPage.tsx
@@ -117,7 +117,7 @@ export const DashboardPage: React.FC<IDashboardPageProps> = props => {
     }
 
     const onSliceClick = (id: string) => {
-        history.push(`/departments/${id}`)
+        history.push(`/departments/detail/${id}`)
     }
 
     const getDeptTables = () => {
