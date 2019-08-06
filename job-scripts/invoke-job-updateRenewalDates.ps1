c834122134ca4b1a5a7548671c82b03165ee7b30
Joseph Turnbull <joseph.turnbull@cqlcorp.com>
2019-08-06 12:14:47 -0400

    Revert "fixing bug with employee prep"
    This reverts commit 2bfaa28a3f96a55bc3632ac1293b47ce5aff94c6.

--- job-scripts/invoke-job-updateRenewalDates.ps1
+++
@@ -1,3 +0,0 @@
-. .\inject-job-variables.ps1
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest $ITInventoryRoot/api/job/UpdateRenewalDates -Method PATCH -Headers @{"token"=$JobsAuthToken}
