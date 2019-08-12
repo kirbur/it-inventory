3744a9a54655e238caba86102661a0a684338be6
Kirstin Burns <kirstin.burns@cqlcorp.com>
2019-08-12 15:52:03 -0400

    removed picture input from program detail edit

--- src/frontend-admin/src/components/pages/ProgramDetailPage/ProgramDetailEditPage.tsx
+++ src/frontend-admin/src/components/pages/ProgramDetailPage/ProgramDetailEditPage.tsx
@@ -1,21 +1,19 @@
-import React, {useState, useEffect, useContext} from 'react'
-import {AxiosService} from '../../../services/AxiosService/AxiosService'
+import React, { useState, useEffect, useContext } from 'react'
+import { AxiosService } from '../../../services/AxiosService/AxiosService'
 
 // Components
-import {Button} from '../../reusables/Button/Button'
-import {PictureInput} from '../../reusables/PictureInput/PictureInput'
-import {ProgramForm, IProgramFormInputs} from '../../reusables/ProgramForm/ProgramForm'
-import {DropdownList} from '../../reusables/Dropdown/DropdownList'
-import {History} from 'history'
-import {match} from 'react-router-dom'
-import {BackButton} from '../../reusables/BackButton/BackButton'
+import { Button } from '../../reusables/Button/Button'
+import { ProgramForm, IProgramFormInputs } from '../../reusables/ProgramForm/ProgramForm'
+import { DropdownList } from '../../reusables/Dropdown/DropdownList'
+import { History } from 'history'
+import { match } from 'react-router-dom'
+import { BackButton } from '../../reusables/BackButton/BackButton'
 
 // Utils
-import {concatStyles as s} from '../../../utilities/mikesConcat'
-import { putUploadImage } from '../../../utilities/UploadImage';
+import { concatStyles as s } from '../../../utilities/mikesConcat'
 
 // Context
-import {LoginContext, ThemeContext} from '../../App/App'
+import { LoginContext, ThemeContext } from '../../App/App'
 
 // Styles
 import styles from './ProgramDetailEditPage.module.css'
@@ -24,31 +22,30 @@ import dropdownStyles from '../../reusables/Dropdown/Dropdown.module.css'
 // Types
 interface IProgramDetailEditPageProps {
     history: History
-    match: match<{id: string}>
+    match: match<{ id: string }>
 }
 
 // Helpers
 
 // Primary Component
 export const ProgramDetailEditPage: React.SFC<IProgramDetailEditPageProps> = props => {
-    const {history, match} = props
+    const { history, match } = props
 
-    const {loginContextVariables} = useContext(LoginContext)
+    const { loginContextVariables } = useContext(LoginContext)
     const { isDarkMode } = useContext(ThemeContext)
 
     const axios = new AxiosService(loginContextVariables)
-    const [progData, setProgData] = useState<{name: string; employee: string; dateBought: string}>({
+    const [progData, setProgData] = useState<{ name: string; employee: string; dateBought: string }>({
         name: '',
         employee: '',
         dateBought: '',
     })
 
-    const [employeeDropdown, setEmployeeDropdown] = useState<{name: string; id: number}[]>()
-    const [selectedEmployee, setSelectedEmployee] = useState<{name: string; id: number}>()
+    const [employeeDropdown, setEmployeeDropdown] = useState<{ name: string; id: number }[]>()
+    const [selectedEmployee, setSelectedEmployee] = useState<{ name: string; id: number }>()
 
     //input feild states:
     const [programInput, setProgramInput] = useState<IProgramFormInputs>()
-    const [imgInput, setImgInput] = useState<File>()
 
     useEffect(() => {
         axios
@@ -61,15 +58,15 @@ export const ProgramDetailEditPage: React.SFC<IProgramDetailEditPageProps> = pro
                 })
 
                 setProgramInput({
-                    name: {value: data[0].programName, changed: false},
-                    programName: {value: data[0].programName, changed: false},
-                    description: {value: data[0].description, changed: false},
-                    renewalDate: {value: new Date(data[0].renewalDate), changed: false},
-                    purchaseDate: {value: new Date(data[0].dateBought), changed: false},
-                    purchaseLink: {value: data[0].programPurchaseLink, changed: false},
-                    licenseKey: {value: data[0].programLicenseKey ? data[0].programLicenseKey : '', changed: false},
-                    cost: {value: data[0].programCostPerYear / (12 / data[0].monthsPerRenewal), changed: false},
-                    flatCost: {value: data[0].programFlatCost ? data[0].programFlatCost : 0, changed: false},
+                    name: { value: data[0].programName, changed: false },
+                    programName: { value: data[0].programName, changed: false },
+                    description: { value: data[0].description, changed: false },
+                    renewalDate: { value: new Date(data[0].renewalDate), changed: false },
+                    purchaseDate: { value: new Date(data[0].dateBought), changed: false },
+                    purchaseLink: { value: data[0].programPurchaseLink, changed: false },
+                    licenseKey: { value: data[0].programLicenseKey ? data[0].programLicenseKey : '', changed: false },
+                    cost: { value: data[0].programCostPerYear / (12 / data[0].monthsPerRenewal), changed: false },
+                    flatCost: { value: data[0].programFlatCost ? data[0].programFlatCost : 0, changed: false },
                     monthsPerRenewal: {
                         value: data[0].monthsPerRenewal,
                         changed: false,
@@ -78,25 +75,20 @@ export const ProgramDetailEditPage: React.SFC<IProgramDetailEditPageProps> = pro
                     hasRecurringCost: data[0].programCostPerYear > 0 ? true : false,
                 })
 
-                const employees: {name: string; id: number}[] = []
-                data[0].listOfEmployees.map((i: {employeeName: string; employeeId: number}) =>
+                const employees: { name: string; id: number }[] = []
+                data[0].listOfEmployees.map((i: { employeeName: string; employeeId: number }) =>
                     employees.push({
                         name: i.employeeName,
                         id: i.employeeId,
                     })
                 )
                 setEmployeeDropdown(employees)
-                setSelectedEmployee({name: data[0].employeeName, id: data[0].employeeId})
+                setSelectedEmployee({ name: data[0].employeeName, id: data[0].employeeId })
             })
             .catch((err: any) => console.error(err))
     }, [])
 
     async function handleSubmit() {
-        //update image
-        if (imgInput) {
-            putUploadImage(imgInput, `/image/program/${match.params.id}`, axios)
-        }
-
         //update program info
         if (programInput) {
             var updateProgram = {
@@ -123,7 +115,7 @@ export const ProgramDetailEditPage: React.SFC<IProgramDetailEditPageProps> = pro
 
             await axios.put(`update/program/${match.params.id}`, updateProgram).catch((err: any) => console.error(err))
 
-            history.push({pathname: `/programs/detail/${match.params.id}`, state: {prev: history.location}})
+            history.push({ pathname: `/programs/detail/${match.params.id}`, state: { prev: history.location } })
         }
     }
 
@@ -133,13 +125,10 @@ export const ProgramDetailEditPage: React.SFC<IProgramDetailEditPageProps> = pro
 
             <div className={styles.firstColumn}>
                 <BackButton history={history} className={styles.backButton} />
-                <div className={styles.imgContainer}>
-                    <PictureInput setImage={setImgInput} image={imgInput} />
-                </div>
             </div>
             {/* column 2 */}
             <div className={styles.secondColumn}>
-                <div className={s(styles.title, styles.paddingBottom, isDarkMode ? styles.dark : {})}>Program Information</div>
+                <div className={s(styles.title, styles.paddingBottom, isDarkMode ? styles.dark : {})}>{progData.name + ' Copy ' + match.params.id + ' '} Information</div>
 
                 {programInput && <ProgramForm state={programInput} setState={setProgramInput} />}
 
@@ -150,7 +139,7 @@ export const ProgramDetailEditPage: React.SFC<IProgramDetailEditPageProps> = pro
                         <div className={s(dropdownStyles.dropdownContainer, styles.employeeDropdownContainer)}>
                             {employeeDropdown && (
                                 <DropdownList
-                                    triggerElement={({isOpen, toggle}) => (
+                                    triggerElement={({ isOpen, toggle }) => (
                                         <button onClick={toggle} className={dropdownStyles.dropdownButton}>
                                             <div
                                                 className={s(
