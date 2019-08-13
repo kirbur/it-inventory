8e636e547407e6e51e62b3a3c5d8b4ba931e2a6b
Joseph Turnbull <joseph.turnbull@cqlcorp.com>
2019-08-13 08:40:31 -0400

    Accessing program photos location

--- src/backend-api/Controllers/DetailController.cs
+++ src/backend-api/Controllers/DetailController.cs
@@ -546,8 +546,12 @@ namespace backend_api.Controllers
             // if the program does exist...
             else
             {
+                var ID = _context.Program
+                    .Where(x => x.ProgramName == prog.ProgramName)
+                    .Select(x => x.ProgramId)
+                    .FirstOrDefault();
                 // Partial path for picture
-                string picture = $"/image/program/{id}";
+                string picture = $"/image/program/{ID}";
 
 
                 // holds the employee name for concatenation purposes 
