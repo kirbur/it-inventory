f9106bf09db7cff6bd54203b59f128166f8c11d9
Alex McDowell <alex.mcdowell@cqlcorp.com>
2019-08-15 10:11:19 -0400

    updated submit buttons

--- src/frontend-admin/src/components/pages/EmployeeDetailPage/EmployeeDetailEditPage.tsx
+++ src/frontend-admin/src/components/pages/EmployeeDetailPage/EmployeeDetailEditPage.tsx
@@ -764,6 +764,9 @@ export const EmployeeDetailEditPage: React.SFC<IEmployeeDetailEditPageProps> = p
                 <div className={styles.imgContainer}>
                     <PictureInput setImage={setImgInput} image={imgInput} />
                 </div>
+                <div className={styles.submitContainer}>
+                    <Button text='Submit' icon='submit' onClick={handleSubmit} className={styles.submitbutton} />
+                </div>
             </div>
             {/* column 2 */}
             <div className={styles.secondColumn}>
@@ -1067,7 +1070,7 @@ export const EmployeeDetailEditPage: React.SFC<IEmployeeDetailEditPageProps> = p
                 </div>
 
                 <div className={styles.submitContainer}>
-                    <Button text='Submit' icon='submit' onClick={handleSubmit} className={styles.submitbutton} />
+                    <Button text='Submit' icon='submit' onClick={handleSubmit} />
                 </div>
             </div>
         </div>
